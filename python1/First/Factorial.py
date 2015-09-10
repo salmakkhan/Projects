50e24baf6ad3bad86c76551dfe30d97a30e975bd
salma <salma@192.168.5.31>
2015-09-10 08:50:58 -0400

    python

---
+++ python1/First/Factorial.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 8, 2015
+
+@author: salma
+'''
+def main():
+    import math     
+    
+ 
+    n=0
+   
+ 
+    for n in range(20):
+        f= math.factorial (n)
+        print(n,"!=",f)
+    
+main()
---
+++ python1/First/TempCF.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 8, 2015
+
+@author: salma
+'''
+
+
+
+
+def main():
+    tempC = input("Input the temperature in Celsius: ")
+    tempF = (tempC * 9.0/5.0 + 32)
+    print(tempF)
+    
+    
+main()
